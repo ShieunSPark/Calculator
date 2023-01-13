aef4d6e8cbf483aa9bddc2664629a54f190c52e1
Shieun <121051962+ShieunSPark@users.noreply.github.com>
2023-01-12 22:30:43 -0500

    Added logic for special operations

--- script.js
+++ script.js
@@ -29,15 +29,54 @@ const operate = (operator, num1, num2) => {
 
 // Track text on calculator
 const itemPressed = document.getElementById('input');
+itemPressed.textContent = "0";
 
 // Functionality for numbers
 const btns = document.querySelectorAll('.buttons.number');
+const special = "0+-*/";
 btns.forEach(btn => btn.addEventListener('click', () => {
-    itemPressed.textContent += btn.textContent;
+    if (special.indexOf(itemPressed.textContent) === -1)
+        itemPressed.textContent += btn.textContent;
+    else
+        itemPressed.textContent = btn.textContent
 }));
 
 // Functionality for "C"
 const clear = document.getElementById('C');
 clear.addEventListener('click', () => {
-    itemPressed.textContent = '';
+    itemPressed.textContent = '0';
+});
+
+// Functionality for operators
+const operators = document.querySelectorAll('.buttons.operators');
+operators.forEach(operator => operator.addEventListener('click', () => {
+    if (operator.textContent === "+") 
+        itemPressed.textContent = "+";
+    else if (operator.textContent=== "-") 
+        itemPressed.textContent = "-";
+    else if (operator.textContent === "*") 
+        itemPressed.textContent = "*";
+    else if (operator.textContent === "/") 
+        itemPressed.textContent = "/";
+}));
+
+// Functionality for special operators
+const inverse = document.getElementById('inverse');
+inverse.addEventListener('click', () => {
+    itemPressed.textContent = 1/parseFloat(itemPressed.textContent);
+});
+
+const square = document.getElementById('square');
+square.addEventListener('click', () => {
+    itemPressed.textContent = Math.pow(parseFloat(itemPressed.textContent), 2);
+});
+
+const root = document.getElementById('root');
+root.addEventListener('click', () => {
+    itemPressed.textContent = Math.sqrt(parseFloat(itemPressed.textContent));
+});
+
+const squareY = document.getElementById('squareY');
+squareY.addEventListener('click', () => {
+    itemPressed.textContent = 'x*y';
 });
