77e20f7eb5d132bfbaff9ce9a48ab9068d57fd5f
Shieun <121051962+ShieunSPark@users.noreply.github.com>
2023-01-18 23:11:38 -0500

    Trying to fix small aesthetics and just breaking things more. So much fun.

--- script.js
+++ script.js
@@ -28,55 +28,66 @@ const operate = (operator, num1, num2) => {
 }
 
 // Track text on calculator
-const itemPressed = document.getElementById('input');
-itemPressed.textContent = "0";
+const display = document.getElementById('display');
+display.textContent = "0";
+
+// Track inputs that show up on display
+let oldNum = 0;
+let newNum = 0;
 
 // Functionality for numbers
 const btns = document.querySelectorAll('.buttons.number');
 const special = "0+-*/";
 btns.forEach(btn => btn.addEventListener('click', () => {
-    if (special.indexOf(itemPressed.textContent) === -1)
-        itemPressed.textContent += btn.textContent;
+    if (special.indexOf(display.textContent) === -1)
+        display.textContent += btn.textContent;
     else
-        itemPressed.textContent = btn.textContent
+        display.textContent = btn.textContent
+    
+    // Reset operator button's color if one was pressed
+    operators.forEach(operator => operator.style.background = "#f0f0f0");
 }));
 
 // Functionality for "C"
 const clear = document.getElementById('C');
 clear.addEventListener('click', () => {
-    itemPressed.textContent = '0';
+    display.textContent = '0';
 });
 
 // Functionality for operators
 const operators = document.querySelectorAll('.buttons.operators');
 operators.forEach(operator => operator.addEventListener('click', () => {
+    // Store original input 
+    oldNum = parseFloat(display.textContent);
+
+    operator.style.background = "gold";
     if (operator.textContent === "+") 
-        itemPressed.textContent = "+";
+        display.textContent = "+";
     else if (operator.textContent=== "-") 
-        itemPressed.textContent = "-";
+        display.textContent = "-";
     else if (operator.textContent === "*") 
-        itemPressed.textContent = "*";
+        display.textContent = "*";
     else if (operator.textContent === "/") 
-        itemPressed.textContent = "/";
+        display.textContent = "/";
 }));
 
 // Functionality for special operators
 const inverse = document.getElementById('inverse');
 inverse.addEventListener('click', () => {
-    itemPressed.textContent = 1/parseFloat(itemPressed.textContent);
+    display.textContent = 1/parseFloat(display.textContent);
 });
 
 const square = document.getElementById('square');
 square.addEventListener('click', () => {
-    itemPressed.textContent = Math.pow(parseFloat(itemPressed.textContent), 2);
+    display.textContent = Math.pow(parseFloat(display.textContent), 2);
 });
 
 const root = document.getElementById('root');
 root.addEventListener('click', () => {
-    itemPressed.textContent = Math.sqrt(parseFloat(itemPressed.textContent));
+    display.textContent = Math.sqrt(parseFloat(display.textContent));
 });
 
 const squareY = document.getElementById('squareY');
 squareY.addEventListener('click', () => {
-    itemPressed.textContent = 'x*y';
+    display.textContent = 'x*y';
 });
